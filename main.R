0e26be0511e4f6d59e43c5be4019f28f9a3d1e68
amaurel <alexandre.maurel@gmail.com>
2018-01-18 09:06:38 +0100

    init

--- main.R
+++ main.R
@@ -1,13 +1,28 @@
 library(tercen)
 library(dplyr)
-library(reshape2)
-
-(ctx = tercenCtx())  %>% 
+ 
+do.scale = function(df, ...){
+  
+  scale.m = try(scale(as.matrix(df$.y), ...), silent = TRUE)
+  
+  if(inherits(scale.m, 'try-error')) {
+    return (data.frame(.ri = integer(),
+                       .ci = integer(),
+                       scale=double()))
+  }  
+  
+  result = as_tibble(scale.m) %>% 
+    rename_all(.funs=function(x) 'scale') %>% 
+    mutate(.ri = df$.ri, .ci = df$.ci)
+  
+  return (result)
+}
+ 
+(ctx = tercenCtx()) %>% 
   select(.ci, .ri, .y) %>% 
-  reshape2::acast(.ci ~ .ri, value.var='.y', fun.aggregate=mean) %>%
-  scale(scale=as.logical(ctx$op.value('scale')), 
-         center=as.logical(ctx$op.value('center'))) %>%
-  as_tibble() %>%
+  group_by(.ri) %>%
+  do(do.scale(., scale=as.logical(ctx$op.value('scale')), 
+              center=as.logical(ctx$op.value('center')))) %>%
   ctx$addNamespace() %>%
   ctx$save()
 
