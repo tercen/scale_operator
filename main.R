3ff82c7d3434aa71e78426a55126318978aca106
Thiago Monteiro <tsmonteiro@gmail.com>
2023-03-30 19:25:46 +0000

    config parameters

--- main.R
+++ main.R
@@ -11,10 +11,8 @@ do.scale = function(y, ci, ...){
     return (double())
   }  
   
-  # browser()
   result <- data.frame("value"=scale.m, ".ci"=ci)
   
-  # return (unlist(as.list(scale.m)))
   return (result)
 }
 
@@ -24,7 +22,11 @@ ctx = tercenCtx()
 df <- ctx$select(c(".ci", ".ri", ".y"))
 dt <- data.table(df)
 dt[ , .(.y = mean(.y)), by = c(".ci",".ri")]
-outDf <- dt[ , c("value", ".ci") := do.scale(.y, .ci), by = c(".ri") ] %>% 
+outDf <- dt[ , c("value", ".ci") := 
+               do.scale(.y, .ci, 
+                        ctx$op.value("scale", as.logical, TRUE),
+                        ctx$op.value("center", as.logical, TRUE)),
+             by = c(".ri") ] %>% 
   select(-.y) %>%
   as.data.frame() %>%
   arrange(.ri, .ci) %>%
@@ -32,30 +34,3 @@ outDf <- dt[ , c("value", ".ci") := do.scale(.y, .ci), by = c(".ri") ] %>%
   relocate(value) %>%
   ctx$addNamespace() %>%
   ctx$save()
-# do.scale = function(df, ...){
-#   
-#   scale.m = try(scale(as.matrix(df$.y), ...), silent = TRUE)
-#   
-#   if(inherits(scale.m, 'try-error')) {
-#     return (data.frame(.ri = integer(),
-#                        .ci = integer(),
-#                        scale=double()))
-#   }  
-#   
-#   result = as_tibble(scale.m) %>% 
-#   rename_all(.funs=function(x) 'scale') %>% 
-#   mutate(.ri = df$.ri, .ci = df$.ci)
-#   
-#   return (result)
-# }
-#  
-# (ctx = tercenCtx()) %>% 
-#   select(.ci, .ri, .y) %>% 
-#   group_by(.ci, .ri) %>% 
-#   summarise(.y = mean(.y)) %>%
-#   group_by(.ri) %>%
-#   do(do.scale(., scale=as.logical(ctx$op.value('scale')), 
-#               center=as.logical(ctx$op.value('center')))) %>%
-#   ctx$addNamespace() %>%
-#   ctx$save()
-
